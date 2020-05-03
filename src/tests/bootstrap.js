4f273166bdce61c9f15d7067969de71ebe673e45
Ed Kelly <kslat3r@potato.local>
2020-05-03 17:15:52 +0100

    Mock complete

---
+++ src/tests/bootstrap.js
@@ -0,0 +1,4 @@
+process.env.USERS_API_URI = 'http://mockify:8001/users';
+process.env.USER_API_URI = 'http://mockify:8001/users/%s';
+process.env.TODOS_API_URI = 'http://mockify:8001/todos';
+process.env.TODOS_USER_API_URI = 'http://mockify:8001/todos?userId=%s';
